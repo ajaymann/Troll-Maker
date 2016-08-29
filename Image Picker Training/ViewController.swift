3ad86f7234f31795c0a4e5031e7945e1210d4f7c
Ajay Mann <ajaymann@ajays-macbook-pro.local>
2016-08-29 20:31:13 +0530

    Added TextField for Top and Bottom Message

--- Image Picker Training/ViewController.swift
+++ Image Picker Training/ViewController.swift
@@ -12,13 +12,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         imageView.contentMode = .ScaleAspectFill
-        imageView.backgroundColor = UIColor.lightGrayColor()
+        setupTextFields(topTextField)
+        setupTextFields(bottomTextField)
+    }
+    
+    func setupTextFields(tf : UITextField) {
+        let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 5.0
+        ]
+        tf.defaultTextAttributes = memeTextAttributes
+        tf.textAlignment = .Center
+        tf.clearsOnBeginEditing =  true
     }
     
     
